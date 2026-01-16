83e80d6905c676dc02b270fda2f9e9ca25c943eb
Richard Fortier <rfortier@ourchateau.net>
2026-01-16 17:21:20 -0500

    Better logging and nuances of TESQuest::SetStage()

--- Code/client/Games/Skyrim/Forms/TESQuest.h
+++ Code/client/Games/Skyrim/Forms/TESQuest.h
@@ -130,7 +130,7 @@ struct TESQuest : BGSStoryManagerTreeForm
     bool EnsureQuestStarted(bool& succeded, bool force);
 
     bool SetStage(uint16_t stage);
-    void ScriptSetStage(uint16_t stage, bool bForce = false);
+    bool ScriptSetStage(uint16_t stage, bool bForce = false);
     void SetStopped();
     bool IsAnyCutscenePlaying();    
 };
